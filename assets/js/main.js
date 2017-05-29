37d7ae2b262600a6cc3060468841b5aaa2bfb472
mireyasantiago <mireyasantiago105@gmail.com>
2017-05-29 02:21:55 -0500

    madales texto,imagen y calendario

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,10 @@ function iniciarMapa(){
     $('.modal').modal();
   });
           
+/* para el datepicker*/
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+        
